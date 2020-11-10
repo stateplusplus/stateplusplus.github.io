37817f0222446313bf2429055aeeb111b413805f
stateplusplus <74169368+stateplusplus@users.noreply.github.com>
2020-11-10 14:32:18 -0600

    Update components.js

--- js/components.js
+++ js/components.js
@@ -4839,13 +4839,6 @@ class FormAJAX extends Form {
 
   constructor(options) {
     super(options);
-    this.inputClassError = 'form__error';
-    this.method = this.$target.attr('method');
-    this.action = this.$target.attr('action');
-    this.messages = {
-      success: this.$target.attr('data-message-success'),
-      error: this.$target.attr('data-message-error')
-    };
     this._validate();
   }
 
@@ -4863,33 +4856,7 @@ class FormAJAX extends Form {
     });
   }
 
-  _submit() {
-    const self = this;
-
-    $.ajax({
-      type: self.$target.attr('method'),
-      url: self.$target.attr('action'),
-      data: self.$target.serialize()
-    }).done(() => {
-      self._createModal({
-        template: self._getModalTemplate({
-          icon: './img/general/icon-success.svg',
-          message: self.messages.success
-        }),
-        onDismiss: () => {
-          self.$target.trigger('reset');
-          self._floatLabels();
-        }
-      });
-    }).fail(() => {
-      self._createModal({
-        template: self._getModalTemplate({
-          icon: './img/general/icon-error.svg',
-          message: self.messages.error
-        })
-      });
-    });
-  }
+  
 
 }
 
